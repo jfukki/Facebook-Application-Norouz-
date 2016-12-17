05ebf683e81b99488d5959a0f8d627ed22a60127
Farrukh Tahir Malik <jfukki@gmail.com>
2016-12-17 16:38:35 +0500

    Facebook Application Norouz
    Digital Menu

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Norouz</title>
+    
+     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Satisfy|Bree+Serif|Candal|PT+Sans">
+     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+     
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+<!--    Header  -->
+    
+   <section id="banner" class="wow bounceInUp"  data-wow-duration="4s">
+        <div class="container " >
+        </div>
+    </section>
+    
+<!--    Header  -->
+    
+<!--  Menu Heading  -->
+
+    <section >
+        
+        <div class="conatainer" >
+            <div class="row" > 
+                <div class=" col-md-offset-1 col-md-6 col-lg-6">
+                    <h1 class=" wow bounceInUp" > <span id="menu-lunch">LUNCH</span> <span id="menu-menu">MENU</span> </h1>
+                    <p id="menu-time" class="wow swing"  data-wow-duration="2s">MON-FRI:1PM-4PM</p>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-offset-1 col-lg-12 col-md-12">
+                        <ul id="order-list">
+                            <li class="wow slideInLeft">
+                            
+                            <h3 class="order-h3">CHICKEN KOONEDEH</h3>
+                            <small class="order-list-small">2 Skewers of grilled mince chicken served with aromatic saffron rice.</small>
+                            <span class="order-list-price move">455/-</span>
+                            
+                            </li>
+                            
+                            <li class="wow slideInRight">
+                                <h3 class="order-h3">MUTTON KOOBEDEH</h3>
+                                 <small class="order-list-small">2 Skewers of grilled mince Lamb served with aromatic saffron rice.</small>
+                            <span class="order-list-price move2">550/-</span>
+                            
+                            </li>
+                                
+                            <li class="wow slideInLeft">
+                                <h3 class="order-h3">SOLTANI CHICKEN</h3>
+                               <small class="order-list-small">1 Skewers of Koobedeh & Joojeh each served with aromatic saffron rice.</small>  
+                                 <span class="order-list-price move3">500/-</span>
+                            
+                            </li>
+                            
+                            <li class="wow slideInLeft">
+                                <h3 class="order-h3">JOOJEH</h3>
+                               <small class="order-list-small">Skewer of grilled chicken marinated in saffron served with saffron & zareskh rice.</small>  
+                                 <span class="order-list-price move4">450/-</span>
+                            
+                            </li>
+                            
+                            <li class="wow slideInRight">
+                                <h3 class="order-h3">ZARESHK POLOW</h3>
+                               <small class="order-list-small">Roasted chicken leg / chest piece served & sour      zareshl (barberries) rice.
+                                 <span class="order-list-price move5">400/-</span>
+                               </li>
+                                
+                            
+                        </ul>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+        
+    </section>
+
+
+<!--  Menu Heading  -->
+
+    
+            
+<!--                Special Drink in-->
+
+    <section class="menu-special wow rotateInDownLeft">
+        <div >
+            <p id="menu-special-drink">*ADD SPECIAL DRINK IN <span style="margin-left:20px;">100/-</span> </p>
+        </div>
+    </section>
+
+
+<!--                Special Drink in-->
+                       
+<!--Footer                       -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-offset-1 col-md-3">
+                    
+                    Norouz logo will be here..
+                </div>
+                <div class="col-md-3"><a href="http://www.norouz.pk/" target="_blank"><span class="glyphicon glyphicon-globe"></span>www.norouz.pk</a></div>
+                <div class="col-md-3">
+                <a href="https://www.facebook.com/norouz.pk"  target="_blank">  <span class="fa fa-facebook"></span>/norouz.pk</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+<!--Footer                       -->
+                        
+    <script src="assets/js/wow.min.js"></script>
+    
+              <script>
+              new WOW().init();
+    </script>
+</body>
+</html>
